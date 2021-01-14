df1fc00f66636cfc23a9768372abd5f0d282ea8d
afluttershy <russulaaa@gmail.com>
2021-01-14 06:25:41 +0500

    add postman settings

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,20 @@
-from django.urls import path
+from django.urls import path, re_path, include
 from . import views
+from rest_framework import routers
+from .views import TaskViewSet
+from django.views.decorators.csrf import csrf_exempt
+
+
+router = routers.DefaultRouter()
+router.register(r'tasks', TaskViewSet)
+
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
     path('create', views.create, name='create'),
     path('delete/<int:id>/', views.delete),
-    path('edit/<int:id>/', views.edit)
+    path('edit/<int:id>/', views.edit),
+    path('gettasks', views.gettasks)
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, redirect, HttpResponseRedirect
 from .models import Task
 from .forms import TaskForm
+from rest_framework import viewsets, serializers
+from rest_framework.parsers import json
+from .serializers import TaskSerializer
+from rest_framework.response import Response
+from django.views.generic.edit import UpdateView
+from django.core import serializers
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 from django.http import HttpResponse
 # Create your views here.
 
@@ -54,3 +62,18 @@ def edit(request, id):
             return render(request, "main/edit.html", {"task": task})
     except Task.DoesNotExist:
         return redirect('home')
+
+
+def gettasks(request):
+    tasks = Task.objects.all()
+    tasks_json = serializers.serialize('json', tasks)
+    return HttpResponse(tasks_json, content_type='application/json')
+
+# serializers.serialize('json', tasks, fields=('title', 'task'))
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+
+
